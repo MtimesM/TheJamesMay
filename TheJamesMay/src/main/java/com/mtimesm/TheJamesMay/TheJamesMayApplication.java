04e0a9202e74f75c84cd056228787fc9af71204e
Marton <misik.marton@gmail.com>
2023-08-14 23:17:47 +0200

    Added Springapp skeleton

---
+++ TheJamesMay/src/main/java/com/mtimesm/TheJamesMay/TheJamesMayApplication.java
@@ -0,0 +1,13 @@
+package com.mtimesm.TheJamesMay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheJamesMayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheJamesMayApplication.class, args);
+	}
+
+}
---
+++ TheJamesMay/src/test/java/com/mtimesm/TheJamesMay/TheJamesMayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtimesm.TheJamesMay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheJamesMayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
