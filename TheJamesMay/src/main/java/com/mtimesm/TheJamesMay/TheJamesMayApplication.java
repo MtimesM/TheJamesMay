4b74d258f93660f49814a43ecd153e9c0c63d54d
Marton <misik.marton@gmail.com>
2023-09-06 21:59:48 +0200

    First config props

--- TheJamesMay/src/main/java/com/mtimesm/TheJamesMay/TheJamesMayApplication.java
+++ TheJamesMay/src/main/java/com/mtimesm/TheJamesMay/TheJamesMayApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TheJamesMayApplication {
 
 	public static void main(String[] args) {
+		var dbUrl =
 		SpringApplication.run(TheJamesMayApplication.class, args);
 	}
 
---
+++ TheJamesMay/src/main/java/com/mtimesm/TheJamesMay/config/DatabaseConfigurationProperties.java
@@ -0,0 +1,7 @@
+package com.mtimesm.TheJamesMay.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "database.connection")
+public class DatabaseConfigurationProperties {
+}
